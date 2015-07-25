3a7bd1cf99a2cb4c3c9f88462e41ca7b49bc0088
Nathaniel Watson <nathaniel@nwatson.nz>
2015-07-25 23:50:25 +1200

    Added images and set up responsiveness of the background image.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 // Based on https://gist.github.com/martinwolf/b7e2c380c70e48782925
 
 var gulp = require('gulp'),
-    del = require('del'),
+	del = require('del'),
 	sync = require('browser-sync'),
 	cp = require('child_process'),
 	concat = require('gulp-concat'),
@@ -9,7 +9,8 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
 	runSequence = require('run-sequence'),
-	merge = require('merge-stream');
+	merge = require('merge-stream'),
+	responsive = require('gulp-responsive');
 
 var config = {
 	bowerDir: 'bower_components',
@@ -19,24 +20,49 @@ var config = {
 
 gulp.task('js', function() {
 	return gulp.src(config.src + '/_js/**/*.js')
-    	.pipe(concat('scripts.js'))
-    	.pipe(rename({suffix: '.min'}))
-    	.pipe(uglify())
-    	.pipe(gulp.dest(config.dest + '/js'));
+		.pipe(concat('scripts.js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest(config.dest + '/js'));
 });
 
 gulp.task('css', function() {
-    return gulp.src(config.src + '/_scss/main.scss')
-    	.pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(config.dest + '/css'))
+	return gulp.src(config.src + '/_scss/main.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest(config.dest + '/css'))
 		.pipe(sync.reload({stream:true}));
+});
+
+gulp.task('images', function() {
+	return gulp.src([config.src + '/img/bg.jpg'])
+		.pipe(responsive(
+			[
+				{
+					name: 'bg.jpg',
+					width: 1920,
+					rename: 'bg-1920px.jpg'
+				},{
+					name: 'bg.jpg',
+					width: '100%',
+					rename: 'bg@2x.jpg'
+				},{
+					name: 'bg.jpg',
+					width: 960,
+					rename: 'bg-960px.jpg'
+				},{
+					name: 'bg.jpg',
+					width: 480,
+					rename: 'bg-480px.jpg'
+				}
+			]
+		)).pipe(gulp.dest(config.dest + '/img'));
 });
 
 gulp.task('copy-libs', ['copy-fonts']);
 
 gulp.task('copy-fonts', function() {
-    return gulp.src(config.bowerDir + '/mdi/fonts/**/*.*')
-        .pipe(gulp.dest(config.dest + '/lib/fonts/'))
+	return gulp.src(config.bowerDir + '/mdi/fonts/**/*.*')
+		.pipe(gulp.dest(config.dest + '/lib/fonts/'))
 		.pipe(sync.reload({stream:true}));
 });
 
@@ -48,18 +74,19 @@ gulp.task('build', function(callback) {
 	runSequence('jekyll-build',
 		'css',
 		'js',
-        'copy-libs',
+		'images',
+		'copy-libs',
 		'reload',
 		callback);
 });
 
 gulp.task('jekyll-build', function (done) {
-    return cp.spawn('jekyll', ['build', '--source', config.src, '--destination', config.dest], {stdio: 'inherit'})
-    	.on('close', done);
+	return cp.spawn('jekyll', ['build', '--source', config.src, '--destination', config.dest], {stdio: 'inherit'})
+		.on('close', done);
 });
 
 gulp.task('reload', function () {
-    sync.reload();
+	sync.reload();
 });
 
 gulp.task('browser-sync', ['build'], function() {
@@ -70,7 +97,8 @@ gulp.task('browser-sync', ['build'], function() {
 				"/pencil-stencils": config.dest + "/"
 			}
 		},
-		host: "localhost"
+		host: "localhost",
+		open: false
 	});
 });
 
@@ -79,6 +107,8 @@ gulp.task('watch', ['browser-sync'], function() {
   gulp.watch(config.src + '/_js/**/*.js', ['js']);
   // Watch .scss files
   gulp.watch(config.src + '/**/*.scss', ['css']);
+  // Watch images
+  gulp.watch(config.src + '/img/**/*.*', ['images']);
   // Watch anything Jekyll handles
   gulp.watch([config.src + '/**/*.html', config.src + '/**/*.md'], ['build']);
 });
--- src/_stencils/material-icons.md
+++ src/_stencils/material-icons.md
@@ -2,7 +2,7 @@
 name: Material Icons
 intro: All 800+ of Google's Material Design Icons as a Pencil stencil collection.
 
-image:
+image: material-icons.png
 downloadUrl: https://github.com/nathanielw/Material-Icons-for-Pencil/releases/latest
 sourceUrl: https://github.com/nathanielw/Material-Icons-for-Pencil
 ---
